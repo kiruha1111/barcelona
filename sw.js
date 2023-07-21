1efbcc3e164503a1125e2b96837eb66eeec54911
kiruha1111 <140174529+kiruha1111@users.noreply.github.com>
2023-07-22 01:02:28 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,11 +6,15 @@ const assetUrls = [
     'css/style.css'
 ]
 
-self.addEventListener('install', (event) => {
+self.addEventListener(type: 'install', listener: async event => {
     const cache = await caches.open(staticCacheName)
     await cache.addAll(assetUrls)
 }) 
 
-self.addEventListener('activate', (event) => {
+self.addEventListener(type: 'activate', listener: event => {
     console.log('[SW]: activate')
+})
+
+self.addEventListener(type: 'fetch', listener: event => {
+    console.log('Fetch', event.request.url)
 })
