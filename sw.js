f83f8f971184a579bec5f450c01a9f644ae2de1f
kiruha1111 <140174529+kiruha1111@users.noreply.github.com>
2023-07-22 01:07:56 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -17,4 +17,10 @@ self.addEventListener(type: 'activate', listener: event => {
 
 self.addEventListener(type: 'fetch', listener: event => {
     console.log('Fetch', event.request.url)
+    event.respondWith(cacheFirst(event.request))
 })
+
+async function cacheFirst(request) {
+    const cached = await caches.match(request)
+    return cached ?? await fetch(request)
+}
